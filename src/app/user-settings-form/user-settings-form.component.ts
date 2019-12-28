9819688978f2cb838b4a4495144616472c3f0c31
sam <evaoosyz@gmail.com>
2019-12-28 22:51:32 +0800

    Form Validation

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgModel, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
-
+ 
+  originalUserSettings:UserSettings ={
+    name: null,
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: '測試\n換行...'
+  }
+  // ... 只能用淺複製 ,深複製Lodash
+  userSettings : UserSettings = { ...this.originalUserSettings };
   constructor() { }
 
   ngOnInit() {
   }
-
+  onBlur(field : NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+  
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
 }
